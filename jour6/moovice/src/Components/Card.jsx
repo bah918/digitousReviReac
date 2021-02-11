7b58483a92c52d44263709196e334848f0b611b8
BAH <nafiou6@yahoo.fr>
2021-02-11 17:33:22 +0100

    partie4

--- jour6/moovice/src/Components/Card.jsx
+++ jour6/moovice/src/Components/Card.jsx
@@ -5,7 +5,10 @@ class Card extends React.Component {
   render() {
     return (
       <div>
-          <h1> Card :</h1>
+          <h1> L'affiche : {this.state.props.movies.poster_path}</h1>
+          <h1> Le titre :</h1>
+          <h1> L'année de sortie :</h1>
+          <h1> La description :</h1>
       </div>
     );
   }
